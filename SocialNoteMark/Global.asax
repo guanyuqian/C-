97739f20ab625bbacd5d165751c36b319e669213
CtheSky <CtheSky@126/com>
2015-12-04 23:28:28 +0800

    Initialize the backbone frame

---
+++ SocialNoteMark/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SocialNoteMark.MvcApplication" Language="C#" %>
---
+++ SocialNoteMark/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialNoteMark.Startup))]
+namespace SocialNoteMark
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
