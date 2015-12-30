2dbca5b96ec63298b619acef5d7664e3c25464f2
CtheSky <cthesky@yeah.net>
2015-12-30 12:00:30 +0800

    add/ model UserInfo

---
+++ SocialNoteMark/Models/UserInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SocialNoteMark.Models
+{
+    public class UserInfo
+    {
+        [Key]
+        public int UserInfoID { get; set; }
+
+        public string UserName { get; set; }
+        public string RealName { get; set; }
+        public string Sex { get; set; }
+        public int Age { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
