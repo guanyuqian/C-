de83b70bc05d50df70d7bcef336999477610a523
gaoliyao <347258101@qq.com>
2016-01-05 17:59:54 +0800

    index

--- SocialNoteMark/Controllers/HomeController.cs
+++ SocialNoteMark/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SocialNoteMark.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,13 +7,21 @@ using System.Web.Mvc;
 
 namespace SocialNoteMark.Controllers
 {
+
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        private List<Bulletin> BulletinList;
         public ActionResult Index()
         {
+            //BulletinList = db.Bulletins.SqlQuery("select * from Bulletins where type='0'and flag='0'").ToList();
+            //BulletinList.Reverse();
+            //ViewData["BulletinList"] = BulletinList;
             return View();
+ 
         }
-
+  
+   
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
