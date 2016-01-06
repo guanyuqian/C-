5f14a8b459fc4f01717b280bac48f71dfbdf5312
gaoliyao <347258101@qq.com>
2016-01-06 18:28:26 +0800

    recruitBulletin and index

---
+++ SocialNoteMark/Controllers/BulletinsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialNoteMark.Models
+{
+    public class BulletinsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Bulletins
+        public ActionResult Index()
+        {
+            return View(db.Bulletins.ToList());
+        }
+
+        // GET: Bulletins/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bulletin bulletin = db.Bulletins.Find(id);
+            if (bulletin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bulletin);
+        }
+
+        // GET: Bulletins/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bulletins/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BulletionID,UserName,Type,Flag,Name,Content,CreateDate")] Bulletin bulletin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bulletins.Add(bulletin);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bulletin);
+        }
+
+        // GET: Bulletins/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bulletin bulletin = db.Bulletins.Find(id);
+            if (bulletin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bulletin);
+        }
+
+        // POST: Bulletins/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BulletionID,UserName,Type,Flag,Name,Content,CreateDate")] Bulletin bulletin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bulletin).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bulletin);
+        }
+
+        // GET: Bulletins/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bulletin bulletin = db.Bulletins.Find(id);
+            if (bulletin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bulletin);
+        }
+
+        // POST: Bulletins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bulletin bulletin = db.Bulletins.Find(id);
+            db.Bulletins.Remove(bulletin);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SocialNoteMark/Models/Interest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNoteMark.Models
+{
+    class Interest
+    {
+        [Key]
+        public int InterestID { get; set; }
+
+        [Required]
+        public int UserName { get; set; }
+
+        [Required]
+        public int BulletionID { get; set; }
+    }
+}
